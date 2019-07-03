bbe3c27a287a5b61088eaa8ccf66626798cb0630
Ritvik Nagpal <ritviknagpal48@gmail.com>
2019-07-03 11:39:34 +0530

    Chat room feature added

--- SocketIO/frontend/script.js
+++ SocketIO/frontend/script.js
@@ -2,13 +2,23 @@ let socket = io();
 
 
 $(function () {
-    let msglist = $('#msglist')
-    let sendbtn = $('#sendmsg')
-    let msgbox = $('#msgbox')
-    let loginbox = $('#loginbox')
+    let loginbox = $('#loginbox')   //login elements
     let loginbtn = $('#loginbtn')
     let loginDiv = $('#login-div')
+
     let chatDiv = $('#chat-div')
+
+    let room1_msglist = $('#room1_msglist')  //messages
+    let room2_msglist = $('#room2_msglist')
+
+    let room1_sendbtn = $('#room1_sendmsg')     //buttons
+    let room2_sendbtn = $('#room2_sendmsg')
+
+    let room1_msgbox = $('#room1_msgbox')   //message box(input)
+    let room2_msgbox = $('#room2_msgbox')
+
+    let room1_div = $('#room1')         //room divs
+    let room2_div = $('#room2')
 
     let user = ''
 
@@ -16,24 +26,73 @@ $(function () {
         console.log("Connected " + socket.id)
                
     })
-    socket.on("new_user", function(data){
-        msglist.append($('<li>' + data +" is online" + '</li>'))
+    socket.on('connect', () =>
+    {
+        
     })
-    sendbtn.click(function () {
-        socket.emit('send_msg', {
-            user: user,
-            message: msgbox.val()
-        })
-    })
+    // socket.on("new_user", function(data){
+    //     msglist.append($('<li>' + data +" is online" + '</li>'))
+    // })
 
-    loginbtn.click(function () {
-        user = loginbox.val()
+    loginbtn.click(function () {     //clicking the login button, after which chatting starts,
+        user = loginbox.val()       
         chatDiv.show()
         loginDiv.hide()
         socket.emit('login', {
             user: user
         })
+        var room1 = "room1";
+        var room2 = "room2";
+        socket.emit("room1", room1);
+        socket.emit("room2", room2);
     })
+
+    socket.on("online_message_room1", function(data)   //displaying who is online in room1
+    {
+        room1_msglist.append($('<li>' + data + "</li>" ))
+    })
+
+
+
+
+    socket.on("online_message_room2", function(data) //displaying who is online in room2
+    {
+        room2_msglist.append($('<li>' + data + "</li>" ))
+    })
+
+
+    // sendbtn.click(function () {
+    //     socket.emit('send_msg', {
+    //         user: user,
+    //         message: msgbox.val()
+    //     })
+    // })
+
+
+    room1_sendbtn.click(function()
+    {
+        socket.emit('room1_send_msg', room1_msgbox.val())
+    })
+
+
+    room2_sendbtn.click(function()
+    {
+        socket.emit('room2_send_msg', room2_msgbox.val())
+    })
+
+
+    socket.on("room1_recv_msg", function(message)
+    {
+        room1_msglist.append($('<li>' + message + '</li>'))
+    })
+
+
+    socket.on("room2_recv_msg", function(message)
+    {
+        room2_msglist.append($('<li>' + message + '</li>'))
+    })
+
+
 
     socket.on('recv_msg', function (data) {
         msglist.append($('<li>' + data.user + ': ' + data.message + '</li>'))
--- SocketIO/server.js
+++ SocketIO/server.js
@@ -8,7 +8,10 @@ const app = express();
 const server = http.createServer(app)
 const io = socketio(server)
 
-let usersockets = {}
+let usersockets1 = {};     //for convenience, usersockets1 contains keys as 'user id' and values as 'user name'
+let usersockets2 = {};     // while usersockets2 contains keys as 'user name' and values as 'user ids'
+                            //for convenience as in, when we want to search user  id for a certain user name, and to search user name for a particular user id
+
 
 app.use('/', express.static(path.join(__dirname, 'frontend')))
 
@@ -18,22 +21,54 @@ io.on('connection', (socket) => {
 
     socket.on('login', (data) => {
         // username is in data.user
-        usersockets[data.user] = socket.id
-        socket.broadcast.emit("new_user",data.user)
-        console.log(usersockets)
+        usersockets1[socket.id] = data.user;
+        usersockets2[data.user] = socket.id;
+        console.log(usersockets1);
+        console.log(usersockets2);
     })
     
-    socket.on('send_msg', (data) => {
-        // if we use io.emit, everyone gets it
+    // socket.on('send_msg', (data) => {
+    //     // if we use io.emit, everyone gets it
+    //     // if we use socket.broadcast.emit, only others get it
+    //     if (data.message.startsWith('@')) {
+    //        
+    //         let recipient = data.message.split(':')[0].substr(1)
+    //         let rcptSocket = usersockets[recipient]
+    //         io.to(rcptSocket).emit('recv_msg', data)
+    //     } else {
+    //         io.emit('recv_msg', data)            
+    //     }
+    // })
+    socket.on("room1", (room_name) =>
+    {
+        socket.join(room_name);
+        socket.broadcast.in(room_name).emit("online_message_room1", usersockets1[socket.id] + " has joined the chat" )
+    })
+    socket.on("room2", (room_name) =>
+    {
+        socket.join(room_name);
+        socket.broadcast.in(room_name).emit("online_message_room2", usersockets1[socket.id] + " has joined the chat" )
+    })
+    socket.on("room1_send_msg", function(message)
+    {
+        if (message.startsWith('@')) {
+             //data.message = "@a: hello"
+           // split at :, then remove @ from beginning
+            let recipient = message.split(':')[0].substr(1)
+              // if we use io.emit, everyone gets it
         // if we use socket.broadcast.emit, only others get it
-        if (data.message.startsWith('@')) {
-            //data.message = "@a: hello"
-            // split at :, then remove @ from beginning
-            let recipient = data.message.split(':')[0].substr(1)
-            let rcptSocket = usersockets[recipient]
-            io.to(rcptSocket).emit('recv_msg', data)
+            io.to(usersockets2[recipient]).emit('room1_recv_msg', usersockets1[socket.id] + ": " + message)
         } else {
-            io.emit('recv_msg', data)            
+            io.emit('room1_recv_msg', usersockets1[socket.id] + ": " + message)            
+        }
+    })
+    socket.on("room2_send_msg", function(message)
+    {
+        if (message.startsWith('@')) {
+            let recipient = message.split(':')[0].substr(1)
+            io.to(usersockets2[recipient]).emit('room2_recv_msg', usersockets1[socket.id] + ": " + message)
+        } else {
+            io.emit('room2_recv_msg', usersockets1[socket.id] + ": " + message)            
         }
     })
 
